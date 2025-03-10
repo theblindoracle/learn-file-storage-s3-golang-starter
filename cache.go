bb02b23ec8b62cfeac89ab7c3c707f42bebcb855
Travis Napier <travis@napes.dev>
2025-03-10 11:44:16 -0400

    implement caching

--- cache.go
+++ cache.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func cacheMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "max-age=3600")
+		w.Header().Set("Cache-Control", "no-store")
 		next.ServeHTTP(w, r)
 	})
 }
